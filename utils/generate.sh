c2e7a6a074342ecc9d5c8c4d15937f89e6755697
babasanfour <hamza.abdelhedi@ieee.org>
2022-02-18 08:08:47 -0800

    modify data paths

--- utils/generate.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#SBATCH --account=def-kjerbi
-#SBATCH --nodes=2
-#SBATCH --time=24:10:00
-#SBATCH --mem=128G
-#SBATCH --cpus-per-task=2
-
-source /home/hamza97/venv/bin/activate
-python  generate_csv.py
