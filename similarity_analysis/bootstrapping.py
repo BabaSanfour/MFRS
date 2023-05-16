1f59a282f1a091a82c53556360359a59bb6bfb60
babasanfour <hamza.abdelhedi@ieee.org>
2023-05-16 13:46:17 -0700

    adapt to new pipeline in siiliraty script

--- similarity_analysis/bootstrapping.py
+++ similarity_analysis/bootstrapping.py
@@ -1,89 +1,88 @@
 import copy
-import time
+from tqdm import tqdm
 import numpy as np
 from scipy.stats import pearsonr
-
-import rsatoolbox.rdm as rsrdm
+from utils.config_sim_analysis import meg_sensors
 from rsatoolbox.inference import bootstrap_sample_pattern
+import rsatoolbox.rdm as rsrdm
 
-def get_rdms_vectors(
-        rdms: np.array,
-        dissimilarity_measure: str ="correlation"
-    ):
+def get_rdms_vectors(rdms: np.array, dissimilarity_measure: str = "correlation"):
     """
-        Read RDMs, transform them into an RDM object from rsatoolbox (1-D vector with only upper part of the matrice).
+    Transform RDMs into an RDM object from rsatoolbox.
 
-        Parameters:
-        ---------------
-        rdms                      Model or MEG rdms, size: [N_layers/N_sensors, n_cons, n_cons]
-        dissimilarity_measure     Method to compute the correlation between the RDMs : expecting: correlation
+    Parameters:
+    -----------
+    rdms : np.array
+        Model or MEG RDMs with shape [N_layers/N_sensors, n_cons, n_cons].
+    dissimilarity_measure : str, optional
+        Method to compute the dissimilarity between the RDMs. Default is "correlation".
 
-        returns:
-        ---------------
-        rdms                      an RDM object from rsatoolbox
-    """
-
-    index = np.array([x for x in range(rdms.shape[0])])
-    rdm_descriptors = {'session': [1 for i in range(rdms.shape[0])],
-                   'subj': index,
-                   'index' : index}
-    rdms= rsrdm.RDMs( dissimilarities=rdms,
-                     dissimilarity_measure=dissimilarity_measure,
-                     rdm_descriptors=rdm_descriptors,
-    #                  pattern_descriptors=pattern_descriptors
-                  )
-    return rdms
-
-def eval_bootstrap_pearson(
-            RDM1,
-            RDM2,
-            network_layers: list,
-            model_layers: list,
-            N_bootstrap: int = 100,
-        ):
+    Returns:
+    --------
+    rdms_obj : rsrdm.RDMs
+        RDM object from rsatoolbox.
 
     """
-        Performs bootstrapping over MEG brain RDMs.
 
-        Parameters:
-        ---------------
-        RDM1                 MEG rdms, size: [N_sensors, n_cons, n_cons]
-        RDM2                 Model rdms, size: [n_layers, n_cons, n_cons]
-        network_layers       All of network layers
-        model_layers         Selected layers
-        N_bootstrap          Number of Bootraps
+    index = np.arange(rdms.shape[0])
+    rdm_descriptors = {
+        'session': [1 for _ in range(rdms.shape[0])],
+        'subj': index,
+        'index': index
+    }
 
-        returns:
-        ---------------
-        sensor_r_list        spearman coorelation: Shape [N_layers, N_sensors, N_bootstrap]
+    rdms_obj = rsrdm.RDMs(
+        dissimilarities=rdms,
+        dissimilarity_measure=dissimilarity_measure,
+        rdm_descriptors=rdm_descriptors,
+    )
+
+    return rdms_obj
+
+def eval_bootstrap_pearson(highest_similarity: dict, meg_rdms, model_rdms, N_bootstrap: int = 100):
     """
+    Perform bootstrapping over MEG brain RDMs and compute Pearson correlation.
 
-    sensor_r_list=[]
-    for boot in range(N_bootstrap):
-        sensors_rdm, rdm_idx=bootstrap_sample_pattern(RDM1)
+    Parameters:
+    -----------
+    highest_similarity : dict
+        Dictionary with layer names as keys and sensor information as values.
+    meg_rdms : RDM obj
+        MEG RDMs, size: [N_sensors, n_cons, n_cons].
+    model_rdms : RDM obj
+        Model RDMs, size: [n_layers, n_cons, n_cons].
+    N_bootstrap : int, optional
+        Number of bootstrap iterations. Default is 100.
+
+    Returns:
+    --------
+    bootstrapped_similarity : dict
+        Dictionary containing bootstrapped similarity scores for each layer and sensor type.
+
+    """
+    bootsrapped_similarity= {}
+    meg_rdms=get_rdms_vectors(meg_rdms)
+    model_rdms=get_rdms_vectors(model_rdms)
+    for _ in tqdm(range(N_bootstrap), desc="Bootstrapping"):
+        sensors_rdm, rdm_idx=bootstrap_sample_pattern(meg_rdms)
         nan_idx = ~np.isnan(sensors_rdm.dissimilarities[0])
-        # print(nan_idx.shape)
-        r_list=[[] for i in range(len(model_layers))]
-        RDM2_copy=copy.deepcopy(RDM2)
-        RDM2=RDM2.subsample_pattern("index", rdm_idx)
+        model_rdms_copy=copy.deepcopy(model_rdms)
+        model_rdms=model_rdms.subsample_pattern("index", rdm_idx)
+        for layer_index, (layer_name, sensors_type) in enumerate(highest_similarity.items()):
+            if layer_name not in bootsrapped_similarity:
+                bootsrapped_similarity[layer_name] = {
+                        "grad1": [],
+                        "grad2": [],
+                        "mag": []
+                    }
+            layer_rdm=model_rdms.dissimilarities[layer_index][nan_idx]
+            for sensor_type, results in sensors_type.items():
+                sensor_name = results[1]
+                sensor_meg_idx=meg_sensors.index(sensor_name)
+                sensor_rdm = sensors_rdm.dissimilarities[sensor_meg_idx][nan_idx]
+                boot_sim = np.array(pearsonr(sensor_rdm, layer_rdm))[0]
+                bootsrapped_similarity[layer_name][sensor_type] = (boot_sim, sensor_name)
 
-        for i in range(RDM1.dissimilarities.shape[0]):
-            sensor_rdm = sensors_rdm.dissimilarities[i][nan_idx]
-            k=0
-            test = len(network_layers) == RDM2.dissimilarities.shape[0]
-            for j in range(RDM2.dissimilarities.shape[0]):
-                if test:
-                    if network_layers[j] not in model_layers:
-                        continue
-                    layer_rdm=RDM2.dissimilarities[j][nan_idx]
-                    r_list[k].append(np.array(pearsonr(sensor_rdm, layer_rdm))[0])
-                    k+=1
-                else:
-                    layer_rdm=RDM2.dissimilarities[j][nan_idx]
-                    r_list[k].append(np.array(pearsonr(sensor_rdm, layer_rdm))[0])
-                    k+=1
+        model_rdms=copy.deepcopy(model_rdms_copy)
 
-        sensor_r_list.append(r_list)
-        RDM2=copy.deepcopy(RDM2_copy)
-
-    return np.transpose(np.array(sensor_r_list), (1, 2, 0))
+    return bootsrapped_similarity
