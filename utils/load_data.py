d9ca5b03639671221b2ad4de5036e746a031a35f
Hamza <hamza.abdelhedi@ieee.org>
2023-08-29 22:32:11 -0700

    combine files

--- utils/load_data.py
+++ utils/load_data.py
@@ -3,11 +3,70 @@ import torch
 import torchvision
 import sys
 sys.path.append('../../MFRS')
-from utils.generate_data_from_hdf5 import generate_Dataset_h5, generate_stimuli_h5
 from utils.config import study_path
-
-
 data_path= os.path.join(study_path, 'hdf5/')
+from torch.utils.data import Dataset
+import h5py
+import numpy as np
+
+
+class generate_Dataset_h5(Dataset):
+    """CelebA Dataset stored in hdf5 file"""
+    def __init__(self, dir_path, transform=False):
+        #read the hdf5 file
+        self.file = h5py.File(dir_path, 'r')
+        if len(self.file['images'].shape)==3:
+            self.n_images, self.nx, self.ny = self.file['images'].shape
+        else :
+            self.n_images, self.nx, self.ny, self.nz = self.file['images'].shape
+
+        self.transform = transform
+
+    def __len__(self):
+        """number of images in the file"""
+        return self.n_images
+
+    def __getitem__(self, idx):
+        """return the input image and the associated label"""
+        if len(self.file['images'].shape)==3:
+            input_h5 = self.file['images'][idx,:,:]
+        else:
+            input_h5 = self.file['images'][idx,:,:,:]
+        label_h5 = self.file['meta'][idx]
+        sample = np.array(input_h5.astype('uint8'))
+        label = torch.tensor(int(label_h5))
+        sample = self.transform(sample)
+
+        return sample, label
+
+
+class generate_stimuli_h5(Dataset):
+    """CelebA Dataset stored in hdf5 file"""
+    def __init__(self, dir_path, transform=False):
+        #read the hdf5 file
+        self.file = h5py.File(dir_path, 'r')
+        if len(self.file['images'].shape)==3:
+            self.n_images, self.nx, self.ny = self.file['images'].shape
+        else :
+            self.n_images, self.nx, self.ny, self.nz = self.file['images'].shape
+
+        self.transform = transform
+
+    def __len__(self):
+        """number of images in the file"""
+        return self.n_images
+
+    def __getitem__(self, idx):
+        """return the input image and the associated label"""
+        if len(self.file['images'].shape)==3:
+            input_h5 = self.file['images'][idx,:,:]
+        else:
+            input_h5 = self.file['images'][idx,:,:,:]
+        sample = np.array(input_h5.astype('uint8'))
+        sample = self.transform(sample)
+
+        return sample
+
 
 mean_std = { '1000_30': [0.3612, 0.3056],
               '1000_25': [0.3736, 0.3082],
