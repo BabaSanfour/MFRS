36d1d80f84520eff228a036e36c84752793cef8f
Hamza <hamza.abdelhedi@ieee.org>
2023-09-07 17:33:50 -0700

    redesign function to new analysis

--- similarity_analysis/src/extract_activations.py
+++ similarity_analysis/src/extract_activations.py
@@ -1,180 +1,162 @@
 import os
 import torch
 import torch.nn as nn
-import sys
 import numpy as np
-from typing import Union, List, Dict
-sys.path.append('../../../MFRS')
-from utils.general import save_pickle, load_pickle
-from utils.config import activations_folder, networks
+from typing import Union,  Dict
+import pickle
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '...'))
+from utils.config import activations_folder, layer_types_to_select
 from utils.load_data import Stimuliloader
-from models.inception import inception_v3
-from models.cornet_s import cornet_s
-from models.mobilenet import mobilenet_v2
-from models.resnet import resnet50
-from models.vgg import vgg16_bn
-from models.FaceNet import FaceNet
-from models.SphereFace import SphereFace
-from utils.arg_parser import get_similarity_parser
+from inception import inception_v3
+from cornet_s import cornet_s
+from mobilenet import mobilenet_v2
+from resnet import resnet50
+from vgg import vgg16_bn
+from FaceNet import FaceNet
+from SphereFace import SphereFace
+from utils.arg_parser import get_training_config_parser
 
-activations={}
+import logging  # Import the logging module
 
-def get_activation(name: str) -> None:
-    """ Get the activation of the network without changing the Parameters"""
-    def hook(model, input, output):
-        # detach() is used to detach a tensor from the current computational graph. It returns a new tensor
-        # that doesn't require a gradient. When we don't need a tensor to be traced for the gradient computation,
-        # we detach the tensor from the current computational graph
-        # transform the tensor to numpy (needed type for rdm)
-        # reshape to the proper shape we need to compute rdm
-        # Function from https://discuss.pytorch.org/t/how-can-l-load-my-best-model-as-a-feature-extractor-evaluator/17254/24
-        activations[name] = output.detach().cpu().numpy().reshape(output.shape[0], -1) # output.shape[0] is batch batch_size which should be equal to N_conditions
+# Set up logging configuration
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+# Global variable to store activations
+activations = {}
+
+
+def get_activation_hook(name: str):
+    """
+    Create a hook to capture the activation of a specific layer in the network.
+
+    Parameters:
+        name (str): A name to identify the activation tensor in the activations dictionary.
+
+    Returns:
+        Callable: A hook function that can be registered to capture activations.
+    """
+    def hook(module, input, output):
+        """
+        Hook function to capture the activation tensor.
+
+        This function detaches the tensor from the computation graph, converts it to a NumPy array,
+        and reshapes it for further analysis.
+
+        Parameters:
+            module (torch.nn.Module): The layer to which the hook is attached.
+            input (tuple): Input tensors to the layer.
+            output (torch.Tensor): The output tensor of the layer.
+
+        Returns:
+            None
+        """
+        # Detach the tensor from the computation graph, convert to NumPy, and reshape
+        activation = output.detach().cpu().numpy().reshape(output.size(0), -1)
+        activations[name] = activation
+
     return hook
 
-def model_activations(model: nn.Module, data: torch.Tensor, weights: Union[str, None] = None, method: str = 'all', list_layers: List[Union[int, str, type]] = [],
-                        save: bool = False, file_name: Union[str, None] = None) -> Dict[str, np.ndarray]:
+
+def model_activations(model: nn.Module,
+    data: torch.Tensor,
+    weights: Union[str, None] = None,
+    file_name: Union[str, None] = None
+) -> dict:
     """
     Get a dictionary with the activations of selected layers for a given input tensor.
 
-    Parameters:
-    -----------
-    model: nn.Module
-        The neural network architecture.
-    data: torch.Tensor
-        The input tensor to the network.
-    weights: str
-        The path to the trained model weights. If set to None, the model will use random weights.
-    method: str, optional
-        The method to select the layers whose activations to compute. Must be one of 'all', 'index', 'list', or 'type'.
-        Default is 'all'.
-    list_layers: list, optional
-        A list of layers to get their activations. The structure of the list depends on the value of 'method':
-        - if 'method' is 'index': a list of layer indices (integers).
-        - if 'method' is 'list': a list of layer names (strings).
-        - if 'method' is 'type': a list of layer types (nn.Module subclasses).
-    save: bool, optional
-        Whether to save the computed activations to a file. Default is False.
-    file_name: str, optional
-        The name of the file to save the activations to. This parameter is only used if 'save' is set to True.
+    Args:
+        model (nn.Module): The neural network architecture.
+        data (torch.Tensor): The input tensor to the network.
+        weights (str): The path to the trained model weights. If set to None, the model will use random weights.
+        file_name (str, optional): The name of the file to save the activations to.
+            This parameter is only used if 'save' is set to True.
 
     Returns:
-    --------
-    activations: dict
-        A dictionary with layers names as keys and their activations as values.
-        Each activation value is a numpy array of shape [n_cons, N_neurons_in_layer], where 'n_cons' is the number of
-        examples in the input tensor and 'N_neurons_in_layer' is the number of neurons in the corresponding layer.
+        dict: A dictionary with layer names as keys and their activations as values.
+              Each activation value is a numpy array of shape [n_cons, N_neurons_in_layer], where 'n_cons' is the
+              number of examples in the input tensor, and 'N_neurons_in_layer' is the number of neurons in the
+              corresponding layer.
     """
-
-    if method not in ["all", "index", "list", "type"]:
-        raise ValueError("Invalid value for 'method'. Must be one of 'all', 'index', 'list', or 'type'.")
-
-    if method in ["index", "list", "type"] and not list_layers:
-        raise ValueError("List of layers to select is empty.")
-
-    if method == "index" and not all(isinstance(item, int) for item in list_layers):
-        raise ValueError("For method 'index', the list_layers must contain integers only.")
-
-    if method == "index" and max(list_layers) >= len(list(model.named_modules())):
-        raise ValueError("One or more of the provided indices exceeds the number of layers in the model.")
-
-    if method == "list" and not all(isinstance(item, str) for item in list_layers):
-        raise ValueError("For method 'list', the list_layers must contain strings only.")
-
-    if method == "list" and not all(elem in [item[0] for item in list(model.named_modules())] for elem in list_layers):
-        raise ValueError("One or more of the provided layer names do not exist in the network.")
-
-    if method == "type" and not all(isinstance(item, type) and issubclass(item, nn.Module) for item in list_layers):
-        raise ValueError("For method 'type', the list_layers must contain nn.Module subclasses only.")
-
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    if weights!= 'None':
-        model.load_state_dict(torch.load(weights, map_location=torch.device(device)))
+
+    # Load model weights if provided
+    if weights and weights != 'None':
+        model.load_state_dict(torch.load(weights, map_location=device))
     else:
-        print("\nFailed to load model weights.\n")
-        print("\nContinuing with random weights.\n")
+        logger.warning("Failed to load model weights. Continuing with random weights.")
+
     model.eval()
-    model.train(False)
+    activations.clear()  # Clear previous activations
 
-    if method == "index":
-        for idx, layer_params in enumerate(model.named_modules()):
-            name, layer = layer_params[0], layer_params[1]
-            if idx in list_layers:
-                layer.register_forward_hook(get_activation(name))
-    elif method=="list":
-        for name, layer in model.named_modules():
-            if name in list_layers:
-                layer.register_forward_hook(get_activation(name))
-    elif method=="type":
-        for name, layer in model.named_modules():
-            if type(layer) in list_layers:
-                layer.register_forward_hook(get_activation(name))
-    else:
-        for name, layer in model.named_modules():
-            layer.register_forward_hook(get_activation(name))
+    # Register hooks for selected layer types
+    for name, layer in model.named_modules():
+        if type(layer) in layer_types_to_select:
+            layer.register_forward_hook(get_activation_hook(name))
 
-    ouptput = model(data)
-    if save:
-        save_pickle(activations, file_name)
+    with torch.no_grad():
+        output = model(data)
 
-    return activations #activation is defined above
+    with open(file_name, 'wb') as f:
+        pickle.dump(activations, f)
 
-def extract_activations(num_images: int, stimuli_file: str, model_name: str, model: nn.Module, 
-        layers: List[str], weights: str, method: str, save: bool, activ_type: str = "trained") -> Dict[str, np.ndarray]:
+    return activations
+
+
+def extract_activations(
+    model_name: str,
+    analysis_type: str,
+    model: nn.Module,
+    weights: str,
+    activ_type: str = "trained"
+) -> Dict[str, np.ndarray]:
     """
     Extracts the activations of the specified layers for the given stimuli using the specified model.
-    
-    Parameters:
-    -----------
-    num_images : int
-        The number of images to be used as stimuli.
-    stimuli_file_name : str
-        The name of the stimuli file.
-    model_name : str
-        The name of the model.
-    model : torch.nn.Module
-        The model.
-    layers : list
-        The names of the layers to extract the activations from.
-    weights : str, optional
-        The path to the model weights.
-    method : str, optional
-        The method for extracting activations.
-    save : bool, optional
-        Whether or not to save the extracted activations.
-        
+
+    Args:
+        model_name (str): The name of the model.
+        analysis_type (str): The type of analysis.
+        model (nn.Module): The model.
+        weights (str): The path to the model weights.
+        activ_type (str, optional): Trained or untrained model.
+
     Returns:
-    --------
-    dict
-        A dictionary containing the activations of the specified layers for the given stimuli.
+        dict: A dictionary containing the activations of the specified layers for the given stimuli.
     """
-    images = Stimuliloader(num_images, stimuli_file)
+    images = Stimuliloader(450, "Stimuli")
     images = next(iter(images))
-    activations_file = os.path.join(activations_folder, f"{model_name}_{stimuli_file}_activations_main_{activ_type}.pkl")
+    activations_file = os.path.join(activations_folder, f"{model_name}_{analysis_type}_activations_{activ_type}.pkl")
+
     if os.path.isfile(activations_file):
-        print(f"Main activations file (data: {model_name}) for {stimuli_file} already exists!!!")
-        activ = load_pickle(activations_file)
+        logger.info(f"Activations file ({model_name}) for {analysis_type} already exists!!!")
+        with open(activations_file, 'rb') as file:
+            activations = pickle.load(file)
     else:
-        activ = model_activations(model, images, weights, method, layers, save, file_name=activations_file)
-    activations_file = os.path.join(activations_folder, f"{model_name}_{stimuli_file}_activations_model_{activ_type}.pkl")
-    if os.path.isfile(activations_file):
-        print(f"Model activations file (data: {model_name}) for {stimuli_file} already exists!!!")
-        model_activ = load_pickle(activations_file)
-    else:
-        model_activ = {}
-        model_activ["model"] = np.concatenate(list(activ.values()), axis=1)
-        if save:
-            save_pickle(model_activ, activations_file)
-    return activ, model_activ
+        logger.info(f"Activations file ( {model_name}) for {analysis_type} does not exist. Creating it...")
+        activations = model_activations(model, images, weights, file_name=activations_file)
+        logger.info(f"Activations file ({model_name}) for {analysis_type} created.")
+
+    return activations
+
 
 if __name__ == '__main__':
-    parser = get_similarity_parser()
+    parser = get_training_config_parser()
     args = parser.parse_args()
-    model_cls = { "cornet_s": cornet_s, "resnet50": resnet50, "mobilenet": mobilenet_v2,  "vgg16_bn": vgg16_bn, 
-                 "inception_v3": inception_v3, "FaceNet": FaceNet, "SphereFace": SphereFace}[args.model_name]
+    model_name = args.model
+    # Create the model
+    model_cls = {
+        "cornet_s": cornet_s,
+        "resnet50": resnet50,
+        "mobilenet": mobilenet_v2,
+        "vgg16_bn": vgg16_bn,
+        "inception_v3": inception_v3,
+        "FaceNet": FaceNet,
+        "SphereFace": SphereFace
+    }[model_name]
 
-    model = model_cls(False, 1000, 1)
-    list_layers = networks[args.model_name]
+    model = model_cls(False, 1000, args.n_input_channels)
+
     # Call extract_activations
-    activs = extract_activations(args.cons, args.stimuli_file_name, args.model_name, model, list_layers, args.weights, args.method, args.save, "trained")
-    model = model_cls(False, 1000, 1)
-    activs = extract_activations(args.cons, args.stimuli_file_name, args.model_name, model, list_layers, "None", args.method, args.save, "untrained")
+    activations = extract_activations(model_name, args.analysis_type, model, args.in_weights, args.activ_type)
