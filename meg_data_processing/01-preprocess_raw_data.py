2302dad015192d2f6fb7c4ff386851c96183e7e9
Hamza <hamza.abdelhedi@ieee.org>
2023-10-04 07:47:29 -0700

    add overwrite and remove if cdt

--- meg_data_processing/01-preprocess_raw_data.py
+++ meg_data_processing/01-preprocess_raw_data.py
@@ -34,71 +34,68 @@ def run_maxwell_filter(subject_id: int) -> None:
 
     for run in range(1, 7):
         raw_out = os.path.join(meg_dir, subject, f'run_{run:02d}_filt_raw.fif')
-        if os.path.exists(raw_out):
-            print(f"The file {os.path.basename(raw_out)} already exists!")
-        else:
-            raw_in = os.path.join(study_path, 'ds000117', subject, 
-                                'ses-meg/meg', f'sub-{subject_id:02d}_ses-meg_task-facerecognition_run-{run:02d}_meg.fif')
-            raw = mne.io.read_raw_fif(raw_in, verbose=False)
-            raw.set_channel_types({
-                    'EEG061': 'eog',
-                    'EEG062': 'eog',
-                    'EEG063': 'ecg',
-                    'EEG064': 'misc'
-                })  # EEG064 free floating el.
-            raw.rename_channels({
-                'EEG061': 'EOG061',
-                'EEG062': 'EOG062',
-                'EEG063': 'ECG063'
-            })
-            raw.fix_mag_coil_types()
+        raw_in = os.path.join(study_path, 'ds000117', subject, 
+                            'ses-meg/meg', f'sub-{subject_id:02d}_ses-meg_task-facerecognition_run-{run:02d}_meg.fif')
+        raw = mne.io.read_raw_fif(raw_in, verbose=False)
+        raw.set_channel_types({
+                'EEG061': 'eog',
+                'EEG062': 'eog',
+                'EEG063': 'ecg',
+                'EEG064': 'misc'
+            })  # EEG064 free floating el.
+        raw.rename_channels({
+            'EEG061': 'EOG061',
+            'EEG062': 'EOG062',
+            'EEG063': 'ECG063'
+        })
+        raw.fix_mag_coil_types()
 
-            # Read bad channels from the MaxFilter log.
-            bads =[]
-            with open( os.path.join(study_path, 'ds000117/derivatives/meg_derivatives', subject, 
-                                'ses-meg/meg', f'sub-{subject_id:02d}_ses-meg_task-facerecognition_run-{run:02d}_proc-sss_log.txt')) as fid:
-                for line in fid:
-                    if line.startswith('Static bad channels'):
-                        chs = line.split(':')[-1].split()
-                        bads = [f'MEG{int(ch) :04d}' for ch in chs]
-                        break
-            raw.info['bads'] += bads
+        # Read bad channels from the MaxFilter log.
+        bads =[]
+        with open( os.path.join(study_path, 'ds000117/derivatives/meg_derivatives', subject, 
+                            'ses-meg/meg', f'sub-{subject_id:02d}_ses-meg_task-facerecognition_run-{run:02d}_proc-sss_log.txt')) as fid:
+            for line in fid:
+                if line.startswith('Static bad channels'):
+                    chs = line.split(':')[-1].split()
+                    bads = [f'MEG{int(ch) :04d}' for ch in chs]
+                    break
+        raw.info['bads'] += bads
 
-            # Get the head positions from the existing SSS file.
-            # Usually this can be done by saving to a text file with MaxFilter
-            # instead, and reading with :func:`mne.chpi.read_head_pos`.
-            raw_sss = mne.io.read_raw_fif(os.path.join(study_path, 'ds000117/derivatives/meg_derivatives', subject, 
-                                'ses-meg/meg', f'sub-{subject_id:02d}_ses-meg_task-facerecognition_run-{run:02d}_proc-sss_meg.fif'), verbose='error')
-            chpi_picks = mne.pick_types(raw_sss.info, meg=False, chpi=True)
-            assert len(chpi_picks) == 9
-            head_pos, t = raw_sss[chpi_picks]
-            # Add first_samp.
-            t = t + raw_sss.first_samp / raw_sss.info['sfreq']
-            # The head positions in the FIF file are all zero for invalid positions so let's remove them, and then concatenate our times.
-            mask = (head_pos != 0).any(axis=0)
-            head_pos = np.concatenate((t[np.newaxis], head_pos)).T[mask]
-            # In this dataset due to old MaxFilter (2.2.10), data are uniformly sampled at 1 Hz, so we save some processing time in maxwell_filter by downsampling.
-            skip = int(round(raw_sss.info['sfreq']))
-            head_pos = head_pos[::skip]
-            del raw_sss
+        # Get the head positions from the existing SSS file.
+        # Usually this can be done by saving to a text file with MaxFilter
+        # instead, and reading with :func:`mne.chpi.read_head_pos`.
+        raw_sss = mne.io.read_raw_fif(os.path.join(study_path, 'ds000117/derivatives/meg_derivatives', subject, 
+                            'ses-meg/meg', f'sub-{subject_id:02d}_ses-meg_task-facerecognition_run-{run:02d}_proc-sss_meg.fif'), verbose='error')
+        chpi_picks = mne.pick_types(raw_sss.info, meg=False, chpi=True)
+        assert len(chpi_picks) == 9
+        head_pos, t = raw_sss[chpi_picks]
+        # Add first_samp.
+        t = t + raw_sss.first_samp / raw_sss.info['sfreq']
+        # The head positions in the FIF file are all zero for invalid positions so let's remove them, and then concatenate our times.
+        mask = (head_pos != 0).any(axis=0)
+        head_pos = np.concatenate((t[np.newaxis], head_pos)).T[mask]
+        # In this dataset due to old MaxFilter (2.2.10), data are uniformly sampled at 1 Hz, so we save some processing time in maxwell_filter by downsampling.
+        skip = int(round(raw_sss.info['sfreq']))
+        head_pos = head_pos[::skip]
+        del raw_sss
 
-            raw_sss = mne.preprocessing.maxwell_filter(
-                raw, calibration=cal, cross_talk=ctc, st_duration=st_duration,
-                origin=origin, destination=destination, head_pos=head_pos)
+        raw_sss = mne.preprocessing.maxwell_filter(
+            raw, calibration=cal, cross_talk=ctc, st_duration=st_duration,
+            origin=origin, destination=destination, head_pos=head_pos)
 
-            #The data are bandpass filtered (1 - 90 Hz) using linear-phase fir filter with delay compensation
-            # Here we only low-pass MEG (assuming MaxFilter has high-passed the data already)
-            picks_meg = mne.pick_types(raw.info, meg=True, exclude=())
-            raw_sss.filter(
-                None, 90, l_trans_bandwidth='auto', h_trans_bandwidth='auto',
-                filter_length='auto', phase='zero', fir_window='hamming',
-                fir_design='firwin', n_jobs=-1, picks=picks_meg)
-            picks_eog = mne.pick_types(raw.info, meg=False, eog=True)
-            raw_sss.filter(
-                1, None, picks=picks_eog, l_trans_bandwidth='auto',
-                filter_length='auto', phase='zero', fir_window='hann',
-                fir_design='firwin')
-            raw_sss.save(raw_out, overwrite=True)
+        #The data are bandpass filtered (1 - 90 Hz) using linear-phase fir filter with delay compensation
+        # Here we only low-pass MEG (assuming MaxFilter has high-passed the data already)
+        picks_meg = mne.pick_types(raw.info, meg=True, exclude=())
+        raw_sss.filter(
+            None, 90, l_trans_bandwidth='auto', h_trans_bandwidth='auto',
+            filter_length='auto', phase='zero', fir_window='hamming',
+            fir_design='firwin', n_jobs=-1, picks=picks_meg)
+        picks_eog = mne.pick_types(raw.info, meg=False, eog=True)
+        raw_sss.filter(
+            1, None, picks=picks_eog, l_trans_bandwidth='auto',
+            filter_length='auto', phase='zero', fir_window='hann',
+            fir_design='firwin')
+        raw_sss.save(raw_out, overwrite=True)
 
 def run_ica(subject_id: int) -> None:
     """
@@ -125,7 +122,7 @@ def run_ica(subject_id: int) -> None:
             decim=11)
     logger.info(f" Fit {ica.n_components_} components (explaining at least {100 * n_components:.1f}% of the variance)")
     logger.info("Saving ICA solution...")
-    ica.save(ica_name)
+    ica.save(ica_name, overwrite=True)
 
 
 if __name__ == "__main__":
